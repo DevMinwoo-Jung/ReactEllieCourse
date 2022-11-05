766e10f79ef4f6ee1e3b1e8675311a8989e7588a
DEVMINWOO-JUNG <applejung312@gmail.com>
2022-11-05 08:30:00 +0000

    pointer

--- newProjects/start/start/src/components/AppXY.jsx
+++ newProjects/start/start/src/components/AppXY.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './appXY.css';
 
 const AppXY = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
   return (
-    <div>AppXY</div>
-  )
+    <div className='container' onPointerMove={(event) => {
+      setX(event.clientX);
+      setY(event.clientY);
+      console.log(event.clientX, event.clientY);
+    }}>
+      <div className='pointer' 
+      style={{transform: `translate(${x}px, ${y}px)`}}>
+      </div>
+    </div>
+  );
 }
 
 export default AppXY
