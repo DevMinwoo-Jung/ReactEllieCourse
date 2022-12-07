1e774e003fe6acbdd026f6df0219938c5f909e1e
DEVMINWOO-JUNG <applejung312@gmail.com>
2022-12-07 22:00:22 +0900

    mouse event ready

--- newProjects/start/start/src/App.js
+++ newProjects/start/start/src/App.js
@@ -1,31 +1,10 @@
 import './App.css';
-import AppProducts from './components/AppProducts';
+import AppXY from './components/AppXY';
 
 function App() {
-  const profileInfo = {
-    devOne: {
-      name: '김아무개',
-      position: 'back-end',
-      isNew: false,
-      profileUrl: 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80',
-    },
-    devTwo: {
-      name: '정민우',
-      position: 'front-end',
-      isNew: true,
-      profileUrl: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80',
-    },
-    devThree: {
-      name: '푸바푸스',
-      position: '바이킹같당',
-      isNew: false,
-      profileUrl: '',
-    }
-  }
-  console.log(Object.values(profileInfo));
   return (
     <div className="App">
-      <AppProducts/>
+      <AppXY/>
     </div>
   );
 }
---
+++ newProjects/start/start/src/components/AppXY.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppXY = () => {
+  return (
+    <div>AppXY</div>
+  )
+}
+
+export default AppXY
