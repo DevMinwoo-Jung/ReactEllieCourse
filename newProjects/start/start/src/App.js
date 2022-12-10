d340beefe98c40aee2b094fa4839262b4257aee3
DEVMINWOO-JUNG <applejung312@gmail.com>
2022-12-10 15:41:49 +0900

    array...

--- newProjects/start/start/src/App.js
+++ newProjects/start/start/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import AppMentor from './components/AppMentor';
+import AppMentors from './components/AppMentors';
 
 function App() {
   return (
     <div className="App">
-      <AppMentor/>
+      <AppMentors/>
     </div>
   );
 }
---
+++ newProjects/start/start/src/components/AppMentors.jsx
@@ -0,0 +1,76 @@
+
+import React, { useState } from 'react'
+const AppMentor = () => {
+  const [person, setPerson] = useState({
+    name: 'minwoo',
+    title: 'dev',
+    mentor: [
+      {
+      name: 'bob',
+      title: 'senior dev'
+      },
+      {
+      name: 'na',
+      title: 'naanana'
+      },
+      {
+      name: 'bob',
+      title: 'senior dev'
+      },
+      {
+      name: 'na',
+      title: 'naanana'
+      },
+      {
+      name: 'bob',
+      title: 'senior dev'
+      },
+      {
+      name: 'na',
+      title: 'naanana'
+      }      
+  ],
+  })
+
+  const changePerson = () => {
+    const name = prompt('누구를 바꾸고 싶어용');
+    const isExist = person.mentor.filter((element) => element.name === name);
+    console.log(isExist);
+    if(isExist.length === 0) {
+      return alert('존재하지 않는디');
+    }
+    const nameIndex = person.mentor.map((element) => element.name).findIndex((element) => element === name);
+    const changeName = prompt('뭘로 바꾸고 싶어용');
+    console.log(person.mentor[nameIndex].name)
+    console.log(changeName);
+    setPerson({...person,
+        mentor:
+        [
+          {
+            ...person.mentor, ...person.mentor[nameIndex].name = changeName
+          }
+        ]  
+      });
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>{person.name}는 {person.title}</h1>
+        <p>
+          {person.name}의
+          {
+            person.mentor.map((element, index) => (
+              <li key={index}>
+                <span>mentor는 {element.name}이고 title은 {element.title}</span>
+              </li>
+            ))    
+          }
+        </p>
+      </div>
+      <button onClick={changePerson}>이름을 뭘로 바꿀거에용</button>
+    </div>
+  )
+}
+
+export default AppMentor
