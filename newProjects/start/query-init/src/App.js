640c15c3c4b3134c53aae94156dedeb9bfce28d7
DEVMINWOO-JUNG <applejung312@gmail.com>
2022-12-27 20:30:00 +0900

    tanstack query practice

---
+++ newProjects/start/query-init/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import MainProducts from './components/MainProducts';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainProducts />;
+    </QueryClientProvider>
+  )
+}
